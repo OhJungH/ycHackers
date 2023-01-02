a68fd11844c0731b2e4250e03e2225e58171118d
LeeGiHeung <dlrlgmd25@gmail.com>
2023-01-02 16:03:05 +0900

    test

--- ycHackers/src/main/java/com/ych/pjt/util/Constant.java
+++ ycHackers/src/main/java/com/ych/pjt/util/Constant.java
@@ -2,6 +2,9 @@ package com.ych.pjt.util;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.ych.pjt.dao.UserDao;
+
 public class Constant {
 	public static BCryptPasswordEncoder passwordEncoder;
+	public static UserDao udao;
 }
